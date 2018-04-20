79c7cb6f234579fbfd1cd8c3fefc0c5461846216
CipCir <ciprian.ciresaru@ipsos.com>
2018-04-20 17:02:42 +0300

    With Auth

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,41 @@
   <nav>
     <div class="nav-wrapper green">      
       <ul id="nav-mobile" class="right">
-        <li><router-link to="/" >View Dashboard</router-link></li>
-        <li><router-link to="/view/cols" class="">View Cols</router-link></li>        
+        <li v-if="!isLoggedIn"><router-link to="/" >View Dashboard</router-link></li>
+        <li v-if="isLoggedIn"><router-link to="/view/cols" class="">View Cols</router-link></li>  
+        <li v-if="!isLoggedIn"><router-link to="/login" class="">Login</router-link></li>
+        <li v-if="!isLoggedIn"><router-link to="/register" class="">Register</router-link></li>
+        <li v-if="isLoggedIn"><button v-on:click="logout" class="btn black">Logout</button></li>
       </ul>
     </div>
   </nav>
 </template>
+
+<script>
+import firebase from 'firebase';
+export default {
+  name: 'navbar',
+  data() {
+    return {
+      isLoggedIn: false,
+      currentUser: false
+    };
+  },
+  created() {
+    if (firebase.auth().currentUser) {
+      this.isLoggedIn = true;
+      this.currentUser = firebase.auth().currentUser.email;
+    }
+  },
+  methods: {
+    logout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+           this.$router.go({ path: this.$router.path });
+        });
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,102 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Dashboard from '@/components/Dashboard'
-import NewTask from '@/components/NewTask'
-import EditTask from '@/components/EditTask'
-import ViewCols from '@/components/ViewCols'
+import Vue from "vue";
+import Router from "vue-router";
+import Dashboard from "@/components/Dashboard";
+import NewTask from "@/components/NewTask";
+import EditTask from "@/components/EditTask";
+import ViewCols from "@/components/ViewCols";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import firebase from "firebase";
+Vue.use(Router);
 
-Vue.use(Router)
-
-export default new Router({
+let router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'dashboard',
-      component: Dashboard
+      path: "/",
+      name: "dashboard",
+      component: Dashboard,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
-      path: '/new',
-      name: 'new-task',
-      component: NewTask
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
-      path: '/edit/:task_id',
-      name: 'edit-task',
-      component: EditTask
-    }
-    ,
+      path: "/register",
+      name: "register",
+      component: Register,
+      meta: {
+        requiresGuest: true
+      }
+    },
     {
-      path: '/view/cols',
-      name: 'view-cols',
-      component: ViewCols
+      path: "/new",
+      name: "new-task",
+      component: NewTask,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/edit/:task_id",
+      name: "edit-task",
+      component: EditTask,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/view/cols",
+      name: "view-cols",
+      component: ViewCols,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
-})
+});
+
+// Nav Guard
+router.beforeEach((to, from, next) => {
+  // Check for requiresAuth guard
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // Check if NO logged user
+    if (!firebase.auth().currentUser) {
+      // Go to login
+      next({
+        path: "/login",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      // Proceed to route
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    // Check if NO logged user
+    if (firebase.auth().currentUser) {
+      // Go to login
+      next({
+        path: "/",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      // Proceed to route
+      next();
+    }
+  } else {
+    // Proceed to route
+    next();
+  }
+});
+
+export default router;
